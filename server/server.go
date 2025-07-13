1c4d6f5e9e958b1355334f279ec922b03925c177
DKeshavarz <dankeshavarz1075@gmail.com>
2025-07-13 08:50:56 +0330

    Chore: Add storing logic to Server

--- server/server.go
+++ server/server.go
@@ -6,9 +6,9 @@ import (
 	"io"
 	"log"
 
+	"article-processing-microservice/database"
 	"article-processing-microservice/proto"
 	"article-processing-microservice/tagextractor"
-
 )
 
 type ArticleServer struct {
@@ -28,6 +28,18 @@ func (s *ArticleServer) ProcessSingleArticle(ctx context.Context, req *proto.Pro
 		return nil, err
 	}
 
+	// Store article in MongoDB
+	article := &database.Article{
+		Title: req.Article.Title,
+		Body:  req.Article.Body,
+		Tags:  tags,
+	}
+
+	if err := database.StoreArticle(article); err != nil {
+		log.Printf("Error storing article in database: %v", err)
+		// Continue processing even if storage fails
+	}
+
 	return &proto.ProcessArticleResponse{Tags: tags}, nil
 }
 
@@ -35,31 +47,49 @@ func (s *ArticleServer) ProcessArticles(stream proto.ArticleService_ProcessArtic
 	log.Println("Starting concurrent bidirectional streaming processing...")
 
 	for {
-        req, err := stream.Recv()
-        if err == io.EOF {
-            return nil
-        }
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
 
-        if err != nil {
-            return errors.New("error recive stream")
-        }
+		if err != nil {
+			return errors.New("error recive stream")
+		}
 
-  
-        go func(request *proto.ProcessArticleRequest) {
-            tags, err := tagextractor.ExtractTags(request.Article.Body, int(request.N))
-            if err != nil {
-                log.Printf("Error extracting tags: %v", err)
-            }
+		go func(request *proto.ProcessArticleRequest) {
+			tags, err := tagextractor.ExtractTags(request.Article.Body, int(request.N))
+			if err != nil {
+				log.Printf("Error extracting tags: %v", err)
+			}
 
-            if err := stream.Send(&proto.ProcessArticleResponse{Tags: tags}); err != nil {
-                log.Printf("Error sending response: %v", err)
-            }
-        }(req)
-    }
+			// Store article in MongoDB
+			article := &database.Article{
+				Title: request.Article.Title,
+				Body:  request.Article.Body,
+				Tags:  tags,
+			}
+
+			if err := database.StoreArticle(article); err != nil {
+				log.Printf("Error storing article in database: %v", err)
+				// Continue processing even if storage fails
+			}
+
+			if err := stream.Send(&proto.ProcessArticleResponse{Tags: tags}); err != nil {
+				log.Printf("Error sending response: %v", err)
+			}
+		}(req)
+	}
 }
 
-//TODO:
+// GetTopTags retrieves the top N most frequent tags from MongoDB
 func (s *ArticleServer) GetTopTags(ctx context.Context, req *proto.GetTopTagsRequest) (*proto.GetTopTagsResponse, error) {
+	log.Printf("Getting top %d tags from database", req.N)
 
-	return &proto.GetTopTagsResponse{Tags: []string{"nigger"}}, nil
+	topTags, err := database.GetTopTags(int(req.N))
+	if err != nil {
+		log.Printf("Error getting top tags from database: %v", err)
+		return nil, err
+	}
+
+	return &proto.GetTopTagsResponse{Tags: topTags}, nil
 }
